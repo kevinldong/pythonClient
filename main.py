5d7e5dfcc20b88d3c838569843fc6c0335873aff
Kevin Dong <110069590+kevinldong@users.noreply.github.com>
2022-10-05 01:49:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
-import requests
 import argparse
+import requests
+from pathlib import Path
+import sys
+from bs4 import BeautifulSoup
 
-parser = argparse.ArgumentParser(description='uploads or downloads files from web server')
-parser.add_argument('action', type=str, metavar='', help='enter your action')
-parser.add_argument('filename', type=str, metavar='', help='enter the file you would like to use')
-args = parser.parse_args()
 
-action = args.action
-filename = args.filename
+def die(status_code: int, message: str):
+    """ Utility function to print a message and exit"""
+    print(message)
+    sys.exit(status_code)
 
-url = 'http://127.0.0.1:5000/'
-if action == 'd':
-    url01 = requests.get(f'{url}/files/')
-    url02 = requests.get(f'{url}/download/Unit01.png')
-    url03 = requests.get(f'{url}/file3/')
-    if filename == 'Unit01.png':
-        with open('Unit01.png', 'wb') as a:
-            a.write(url01.content)
-    elif filename == 'Unit02.png':
-        with open('Unit02.png', 'wb') as b:
-            b.write(url02.content)
-    elif filename == 'Unit03.png':
-        with open('Unit03.png', 'wb') as c:
-            c.write(url03.content)
-    else:
-        print('that is not an available file')
 
-elif action == 'u':
-    if filename == 'Unit00.png':
-        data = open('Unit00.png', 'rb').read()
-        r = requests.post(url, data=data)
+def main():
+    # Setup argparse
+    parser = argparse.ArgumentParser(description="Client for testing the server/ids")
+    parser.add_argument('action', type=str, choices=['upload', 'download', 'u', 'd'],
+                        help="whether to upload or download a file")
+    parser.add_argument('file', type=str, help="the path of the file to download or upload")
+    parser.add_argument('-o', '--output', type=str, default="out.png", help="location to save the downloaded file")
+    parser.add_argument('url', type=str, help="url to the website being tested")
+    args = parser.parse_args()
 
-else:
-    print('that is not an available action')
+    # Setup variables
+    url = args.url
+
+    # Initialize session
+    session = requests.Session()
+    if args.action == 'upload' or args.action == 'u':
+        # Verify that file exists
+        path = Path(args.file)
+        if not path.is_file():
+            die(1, "ERROR: couldn't find file")
+
+        print(f"[+] Uploading {path.absolute()} to {url}")
+        # Get csrf_token
+        print("[+] Getting csrf_token")
+        homepage = session.get(f"{url}/")
+        if homepage.status_code != 200:
+            die(1, "ERROR: couldn't retrieve homepage")
+        soup = BeautifulSoup(homepage.content, "html.parser")
+        csrf_token = soup.find("input", {"name": "csrf_token"})['value']
+        print("[+] Uploading file")
+        # Upload file
+        files = {'file': open(path.absolute(), "rb")}
+        r = session.post(url, files=files, data={"csrf_token": csrf_token, "submit": True})
+        if r.status_code == 200:
+            print("[+] Successfully uploaded file")
+        else:
+            print("ERROR: failed to upload file")
+
+    if args.action == 'download' or args.action == 'd':
+        print(f"[+] Attempting to download {args.file} from {url}")
+        r = session.get(f"{url}/{args.file}/")
+        if r.status_code != 200:
+            print("ERROR: failed to download file")
+            print(r.text)
+        else:
+            print("[+] Got file; saving")
+            with open(args.output, "wb") as outfile:
+                outfile.write(r.content)
+
+
+if __name__ == "__main__":
+    main()
